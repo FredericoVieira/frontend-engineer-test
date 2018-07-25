f2ce8ac23aa85f52bcd7442176752fb40c292ac9
Frederico Vieira <frederico.vieira@hekima.com>
2018-07-25 00:10:46 -0300

    Adds react-router.

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,11 @@
-import React, {Component} from 'react';
-import {render} from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
 
-class App extends Component {
-  render () {
-    return <p>React says Hello!</p>;
-  }
-}
 
-render(<App/>, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'))
